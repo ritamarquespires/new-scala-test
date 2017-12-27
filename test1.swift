d7963d81f882151d89a01f8f4b599e8ad41d789d
Joao Machado <machado@codacy.com>
2017-12-27 17:39:14 +0000

    Create test1.swift

---
+++ test1.swift
@@ -0,0 +1,22 @@
+//#Patterns: arrow-whitespace
+//#Issue: {"severity": "Info", "line": 17, "patternId": "arrow-whitespace"}
+//#Issue: {"severity": "Info", "line": 17, "patternId": "arrow-whitespace"}
+//#Issue: {"severity": "Info", "line": 22, "patternId": "arrow-whitespace"}
+//#Issue: {"severity": "Info", "line": 22, "patternId": "arrow-whitespace"}
+func onePlusTwo() -> Int {
+  return 1 + 2
+}
+
+names.map() {
+  (name) -> Int in
+  return 1
+}
+
+func onePlusTwo()->Int {
+  return 1 + 2
+}
+
+names.map() {
+  (name)  ->  Int in
+  return 1
+}
