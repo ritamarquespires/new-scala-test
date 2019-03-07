b40e65cec564378eadaf12be19410a86092ed8a3
ritamarquespires <ar.marquespires@gmail.com>
2019-03-07 12:04:28 +0000

    Update awesome.scala

--- awesome.scala
+++ awesome.scala
@@ -1,6 +1,6 @@
 val immutableVariable = 40
 
 //The Scala way using immutability
-val immutableVariablePlus2 = immutableVariable + 2
+val immutableVariablePlus2 = immutableVariable + 2;
 
 println(immutableVariablePlus2)
